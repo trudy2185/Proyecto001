dd106eb6c32dcb6cca1095dde0762333508b12df
thalorudy <thalorudy@192.168.1.18>
2022-04-12 05:52:22 -0500

    Add1

--- src/cibertec/Vender.java
+++ src/cibertec/Vender.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 public class Vender extends JFrame {
 
@@ -31,12 +32,16 @@ public class Vender extends JFrame {
 	 * Create the frame.
 	 */
 	public Vender() {
+		setTitle("Vender");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(302, 36, 89, 23);
+		contentPane.add(btnNewButton);
 	}
-
 }
